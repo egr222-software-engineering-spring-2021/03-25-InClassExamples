c9a983407842b10f61a5b60ab2fef1e5264a00f1
Larry Clement <34249201+lwclement@users.noreply.github.com>
2021-03-24 22:01:23 -0700

    After adding ArrayList-Comparable Starter

---
+++ ArrayList-Comparable-Starter/src/comparable/CalenderDateClient.java
@@ -0,0 +1,9 @@
+package comparable;
+import java.util.Collections;
+
+public class CalenderDateClient {
+    public static void main(String[] args) {
+        //Declare and define list of CalenderDate
+        //Collections.sort(list);
+    }
+}
